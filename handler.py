da62ca32885b3ee93a1ddf5e4fdb34378a634352
MennaEzzelarab <menna.ezzelarab@gmail.com>
2025-12-11 22:12:10 +0200

    new

--- handler.py
+++ handler.py
@@ -1,21 +1,26 @@
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import json
 import os
+from cryptography.hazmat.primitives import hashes
 import base64
 import pyotp
-import requests
-import hashlib
 
 def handle_reg(username, password):
     salt = os.urandom(16) # 16 bytes of random salt
 
-    hash_obj = hashlib.sha256(salt + password.encode())
-    password_hash = hash_obj.digest()
+    kdf = PBKDF2HMAC( # key/hash generated from password
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+    )
     
+    key_hash = kdf.derive(password.encode())
 
     record = {
         "username": username,
         "salt": base64.b64encode(salt).decode(), # store as string,
-        "pass": base64.b64encode(password_hash).decode(),
+        "pass": base64.b64encode(key_hash).decode(),
         "otp_special": pyotp.random_base32()
     }
 
@@ -32,48 +37,33 @@ def handle_log(username, password):
     salt = base64.b64decode(record["salt"])
     password_hash = base64.b64decode(record["pass"])
 
-    hash_obj = hashlib.sha256(salt + password.encode())
-    input_hash = hash_obj.digest()
-    
-
-    if password_hash == input_hash:
-        print("login succ")
+    # Derive key from input password
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000
+    )
+    try:
+        kdf.verify(password.encode(), password_hash) # compare
         return [200, "logok"]
-
-    else:
+    except Exception:
         return [400, "err"]
 
-def handle_otp(type, company):
-    if type == "getsecret":
+def handle_otp(type, username):
+    if(type == "getsecret"):
         try:
-            username = company
             with open(f"users/{username}_vault.json", "r") as f:
                 record = json.load(f)
             
             secret = record["otp_special"]
 
+            # Return the otp secret so the mobile app can compute/verify TOTP
             return [200, secret]
         
-        except Exception as e:
+        except:
             return [400, "err"]
 
-    elif type == "code":
-        code = company
-
-        return [200, code]
-
-    elif type == "verify":
-        # Server URL
-        url = "https://127.0.0.1:4444"
-
-        # Body you want to send
-        data = f"otp:verify:{code}"
-
-        # Send POST request (disable SSL verification for self-signed cert)
-        response = requests.post(url, data=data, verify=False)
-
-        # Print server response
-        print(response.text)
 
 def handle_req(body):
     if ":" not in body:
